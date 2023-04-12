8dadedba42413d5fd4229d83eb043f4edc093503
ZachWolpe <zachcolinwolpe@gmail.com>
2023-04-12 10:30:40 +0800

    enum

---
+++ Enum/enum.py
@@ -0,0 +1,21 @@
+from enum import Enum, auto
+
+class Category(Enum):
+    category_a  = auto()
+    category_b  = auto()
+    category_c  = auto()
+
+# example trigger function
+def Trigger(category: Category):
+    if category == Category.category_a:
+        print(Category.__name__ + ' A!')
+    
+
+
+if __name__ == "__main__":
+    Trigger(Category.category_a)
+    Trigger(Category.category_b)
+    Trigger(Category.category_c)
+
+    for c in Category:
+        print(c.name, '->', c.value)
